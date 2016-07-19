5f99cde0716f028682d0ee5623cf1bca28410567
Daniel Rosowski <daniel.rosowski.extern@postadress.de>
2016-07-19 10:12:11 +0200

    Added MultiplicationProblem

---
+++ app/src/main/java/com/rosowski/mathpet/problem/MultiplicationProblem.java
@@ -0,0 +1,29 @@
+package com.rosowski.mathpet.problem;
+
+import java.util.Random;
+
+import com.rosowski.mathpet.Tuple;
+
+public class MultiplicationProblem implements MathProblem {
+
+    private Tuple factors;
+
+    public MultiplicationProblem(int bound) {
+        Random rand = new Random();
+        int factorLeft = rand.nextInt(bound);
+        int factorRight = rand.nextInt(bound);
+        factors = new Tuple(factorLeft, factorRight);
+    }
+    
+	@Override
+	public Tuple getNumbers() {
+		return factors;
+	}
+
+	@Override
+	public boolean checkAnswer(int answer) {
+		int product = factors.left * factors.right;
+		return product == answer;
+	}
+
+}
---
+++ app/src/test/java/com/rosowski/mathpet/problem/MultiplicationProblemTest.java
@@ -0,0 +1,29 @@
+package com.rosowski.mathpet.problem;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.rosowski.mathpet.Tuple;
+
+public class MultiplicationProblemTest {
+
+    @Test
+    public void should_generate_numbers_below_bound() {
+        int bound = 10;
+        Tuple numbers = new MultiplicationProblem(bound).getNumbers();
+        assertTrue(numbers.left < bound);
+        assertTrue(numbers.right < bound);
+    }
+
+    @Test
+    public void should_check_answer_correctly() {
+        int bound = 10;
+        MathProblem problem = new MultiplicationProblem(bound);
+        Tuple numbers = problem.getNumbers();
+
+        assertFalse(problem.checkAnswer(bound * 2));
+        assertTrue(problem.checkAnswer(numbers.left * numbers.right));
+    }	
+}
